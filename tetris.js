fc92e16d2484d3a16ea3aa380d51e83a8929353a
hotcoldvote <210900411+hotcoldvote@users.noreply.github.com>
2025-09-18 10:56:35 +0100

    upadte sdfsdkjvcksdbcvnsjdvc

--- tetris.js
+++ tetris.js
@@ -64,49 +64,47 @@ function createMatrix(w, h) {
 }
 
 function createPiece(type) {
-    if (type === 'I') {
+    if (type === 'O') {
         return [
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
+            [1, 1],
+            [1, 1],
         ];
-    } else if (type === 'L') {
+    } else if (type === 'I') {
         return [
-            [0, 2, 0],
-            [0, 2, 0],
-            [2, 2, 2],
-        ];
-    } else if (type === 'J') {
-        return [
-            [0, 3, 3],
-            [0, 3, 0],
-            [3, 3, 0],
-        ];
-    } else if (type === 'O') {
-        return [
-            [4, 4],
-            [4, 4],
-        ];
-    } else if (type === 'Z') {
-        return [
-            [5, 5, 5],
-            [5, 0, 5],
-            [0, 0, 0],
+            [0, 2, 0, 0],
+            [0, 2, 0, 0],
+            [0, 2, 0, 0],
+            [0, 2, 0, 0],
         ];
     } else if (type === 'S') {
         return [
-            [0, 6, 0, 0],
-            [0, 6, 0, 0],
-            [6, 6, 0, 0],
-            [0, 6, 0, 0],
+            [0, 0, 0],
+            [0, 3, 3],
+            [3, 3, 0],
+        ];
+    } else if (type === 'Z') {
+        return [
+            [0, 0, 0],
+            [4, 4, 0],
+            [0, 4, 4],
+        ];
+    } else if (type === 'L') {
+        return [
+            [5, 0, 0],
+            [5, 0, 0],
+            [5, 5, 0],
+        ];
+    } else if (type === 'J') {
+        return [
+            [0, 6, 0],
+            [0, 6, 0],
+            [6, 6, 0],
         ];
     } else if (type === 'T') {
         return [
-            [0, 7, 0, 0],
-            [0, 7, 0, 0],
-            [0, 7, 7, 0],
-            [0, 7, 0, 0],
+            [0, 0, 0],
+            [0, 7, 0],
+            [7, 7, 7],
         ];
     }
 }
@@ -197,7 +195,7 @@ function playerMove(offset) {
 }
 
 function playerReset() {
-    const pieces = 'TJLOSZI';
+    const pieces = 'OISZLJT';
     player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
     player.pos.y = 0;
     player.pos.x = (arena[0].length / 2 | 0) -
@@ -255,7 +253,7 @@ function updateScore() {
     if (player.score > highScore) {
         highScore = player.score;
         localStorage.setItem("daletrisHighScore", highScore);
-        document.getElementById('highscore').innerText = highScore;
+        document.getElementById('highscore').innerText = "High Score: " + highScore;
     }
 }
 
